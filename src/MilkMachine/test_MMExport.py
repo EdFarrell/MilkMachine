ecfe551ff8fbe1f02cc76ff867d09147e5d1e878
Peter Robinett <peter@bubblefoundry.com>
2016-12-13 09:25:53 -0800

    Separate into two tests

--- src/MilkMachine/test_MMExport.py
+++ src/MilkMachine/test_MMExport.py
@@ -11,7 +11,7 @@ from MMExport import exportToFile
 import test_MMImport
 
 class TestMMExport(unittest.TestCase):
-    def testExportToFile(self):
+    def testExportToFileWithCamera(self):
         QgsApplication.setPrefixPath("/usr/share/qgis", True)
         qgs = QgsApplication([], False)
         # load providers
@@ -58,27 +58,69 @@ class TestMMExport(unittest.TestCase):
             layer.changeAttributeValue(feature.id(), fields['lookat'], '') # get rid of lookats
         layer.endEditCommand()
 
+        loggerPath = ""
+        exportToFile(
+            activeLayer=layer,
+            audioHREF="",
+            audioOffset=0,
+            exportPath="amsterdam-yymmdd-camera.kml",
+            fields=fields,
+            lastDirectory=".",
+            logger=logger,
+            loggerPath=loggerPath,
+            messageBar=messageBar
+        )
+
+        self.assertTrue(messageBar.gotSuccess)
+
+        # qgs.exitQgis()
+
+    def testExportToFileWithModel(self):
+        QgsApplication.setPrefixPath("/usr/share/qgis", True)
+        qgs = QgsApplication([], False)
+        # load providers
+        qgs.initQgis()
+
+        # shapefile
+        aroundtheblockPath = os.path.join(os.path.abspath("../../sampledata/"), "amsterdam-yymmdd.csv")
+        logger = test_MMImport.mockLogger()
+        messageBar = test_MMImport.MockMessageBar(logger)
+        messageBox = test_MMImport.MockMessageBox
+        layer = loadCSVLayer(
+            dateFormat='yyyy/mm/dd',
+            gpsPath=aroundtheblockPath,
+            logger=logger,
+            mainWindow=None,
+            messageBar=messageBar,
+            messageBox=messageBox
+        )
+        self.assertTrue(layer.isValid())
+
+        # reset the flag
+        messageBar.gotSuccess = False
+
+        fields = test_MMImport.field_indices(layer)
         # if model data should be included
-        # modelTemplate = {'link': None, 'longitude': None, 'latitude': None, 'altitude' : None, 'scale': None}
-        # layer.startEditing()
-        # layer.beginEditCommand("Rendering Editing")
-        # for feature in layer.getFeatures():
-        #     attributes = feature.attributes()
-        #     model = modelTemplate
-        #     model['link'] = 'files/red_sphereC.dae'
-        #     model['longitude'] = attributes[fields['x']]
-        #     model['latitude'] = attributes[fields['y']]
-        #     model['scale'] = 1
-        #     model['altitude'] = attributes[fields['altitude']]
-        #     layer.changeAttributeValue(feature.id(), fields['model'], str(model))
-        # layer.endEditCommand()
+        modelTemplate = {'link': None, 'longitude': None, 'latitude': None, 'altitude' : None, 'scale': None}
+        layer.startEditing()
+        layer.beginEditCommand("Rendering Editing")
+        for feature in layer.getFeatures():
+            attributes = feature.attributes()
+            model = modelTemplate
+            model['link'] = 'files/red_sphereC.dae'
+            model['longitude'] = attributes[fields['x']]
+            model['latitude'] = attributes[fields['y']]
+            model['scale'] = 1
+            model['altitude'] = attributes[fields['altitude']]
+            layer.changeAttributeValue(feature.id(), fields['model'], str(model))
+        layer.endEditCommand()
 
         loggerPath = ""
         exportToFile(
             activeLayer=layer,
             audioHREF="",
             audioOffset=0,
-            exportPath="amsterdam-yymmdd.kml",
+            exportPath="amsterdam-yymmdd-model.kml",
             fields=fields,
             lastDirectory=".",
             logger=logger,
